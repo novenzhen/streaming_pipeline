23908750ce914878ea21dfc79ff7e071db0b2f93
Chameleon Cloud User <cc@rf.novalocal>
2017-11-08 21:27:26 +0000

    updating config

--- config.sh
+++ config.sh
@@ -3,20 +3,21 @@ set -x
 
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 sudo yum-config-manager -y --enable docker-ce-edge
 
 sudo yum-config-manager -y --enable docker-ce-test
 
-sudo yum install docker-ce
+sudo yum -y install docker-ce
 
 sudo systemctl start docker
 
-sudo yum install epel-release
+sudo yum -y  install epel-release
 
 sudo yum -y install python-pip
 
-sudo pip install docker-compose
+pip install --upgrade pip
 
+sudo pip install docker-compose
 
